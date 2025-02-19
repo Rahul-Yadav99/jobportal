5dbee5d170409f550b3c56588e7b2da6e392b63d
rahul <rahul352sj@gmail.com>
2025-02-19 13:23:25 +0530

    first commit

--- router/user.route.js
+++ router/user.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { register, login, updateProfile, logout } = require('../controller/user.controller');
 const isAuthenticated = require('../middleware/isAuthenticated');
+const { singleUpload } = require('../middleware/multer.js')
 
 const router = express.Router()
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
-router.route('/profile/update').post(isAuthenticated, updateProfile);
+router.route('/profile/update').post(isAuthenticated, singleUpload, updateProfile);
 
 module.exports = router
