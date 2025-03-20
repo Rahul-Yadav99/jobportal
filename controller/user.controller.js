f359425bcb24fa935b7722011700ed4fbbb9eb84
rahul <rahul352sj@gmail.com>
2025-03-20 16:34:27 +0530

    final push

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,5 @@
 const User = require("../model/user.model");
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const getDataUri = require("../utils/datauri");
 const cloudinary = require('../utils/cloudinary')
