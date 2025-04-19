9f8f8194282c38cbf5fe49049bb70a60e766dce6
rahul <rahul352sj@gmail.com>
2025-04-19 22:15:17 +0530

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 //middleware
 const corsOptions = {
     // origin: 'http://localhost:5173',
-    origin: ['https://jobportal-frontend-seven.vercel.app'],
+    origin: ['https://jobportal-frontend-seven.vercel.app', 'http://localhost:5173'],
     credentials: true
 }
 app.use(express.json())
