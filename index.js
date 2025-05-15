fdb745041e4a7512a9174a041d7928d7ec881c88
rahul <rahul352sj@gmail.com>
2025-05-15 20:45:51 +0530

    same commit every day

--- index.js
+++ index.js
@@ -11,18 +11,16 @@ const cors = require('cors')
 const app = express()
 
 //middleware
-const corsOptions = {
-    // origin: 'http://localhost:5173',
-    origin: ['https://jobportal-frontend-seven.vercel.app', 'http://localhost:5173'],
-    credentials: true
-}
+// const corsOptions = {
+//     // origin: 'http://localhost:5173',
+//     origin: ['https://jobportal-frontend-seven.vercel.app', 'http://localhost:5173'],
+//     credentials: true
+// }
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(cors(corsOptions))
+app.use(cors())
 
-// API's
-// API's
 // API's
 
 app.use('/api/v1/user', userRoute)
@@ -30,8 +28,8 @@ app.use('/api/v1/company', companyRoute)
 app.use('/api/v1/job', jobRoute)
 app.use('/api/v1/application', applicationRoute)
 
-// app.listen(3000, ()=>{
-//     console.log('Server is running on port 3000')
-// })
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
 
-app.listen('https://jobportal-two-coral.vercel.app')
+// app.listen('https://jobportal-two-coral.vercel.app')
