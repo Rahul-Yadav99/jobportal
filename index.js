206248fda665a2a684b44a20b1b42a34118b924a
rahul <rahul352sj@gmail.com>
2025-05-19 21:33:10 +0530

    same commit every day

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ const corsOptions = {
     origin: ['https://jobportal-frontend-seven.vercel.app', 'http://localhost:5173'],
     credentials: true
 }
-app.use(cors(corsOptions));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(cors())
+app.use(cors(corsOptions));
 
 // API's
 
